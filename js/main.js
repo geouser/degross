abf6f69a27ffa650e73de16de150b508ccf59412
rusyi <olehrusy@gmail.com>
2017-03-04 15:23:17 +0200

    ds

--- js/main.js
+++ js/main.js
@@ -53,21 +53,26 @@ jQuery(document).ready(function($) {
     /*---------------------------
                                 ACTIVATE MENU ITEM OVER CURRENT SECTION
     ---------------------------*/
-    var $sections = $('section');
+    var $sections = $('.anchor-point');
     $(window).scroll(function(){
         var currentScroll = $(this).scrollTop();
         var $currentSection;
         var windowHalf = $(window).height() / 2;
         
+
         $sections.each(function(){
           var divPosition = $(this).offset().top - windowHalf;
           
           if( divPosition - 1 < currentScroll ){
             $currentSection = $(this);
           }
-        var id = $currentSection.attr('id');
-          $('a').removeClass('active');
-          $("[href=#"+id+"]").addClass('active');
+          if ($currentSection) {
+             var id = $currentSection.attr('id');
+              $('a').removeClass('active');
+              $("[href=#"+id+"]").addClass('active');
+          } else {
+                $('a').removeClass('active');
+          }
         })
     });
 
